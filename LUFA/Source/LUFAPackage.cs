c3c12d4f2a595d5f62d923de7dfc4e79a0d32b0d
Dean Camera <dean@fourwalledcubicle.com>
2015-10-06 21:16:14 +1100

    Visual Studio extension lookup via GUID is case-sensitive.

--- LUFA/Source/LUFAPackage.cs
+++ LUFA/Source/LUFAPackage.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using Atmel.Studio.Services;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
